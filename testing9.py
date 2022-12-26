97a298ab08319551a9ecda305b7730ce510256fb
Р20-Максим <r20-maksim@monowork.pro>
2022-12-26 09:17:41 +0300

    Scrollview and click

---
+++ testing9.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()
+link = "https://SunInJuly.github.io/execute_script.html"
+try:
+
+    browser.get(link)
+    button = browser.find_element(By.TAG_NAME, 'button')
+    browser.execute_script("window.scrollBy(0, 100);")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+finally:
+    browser.quit()
